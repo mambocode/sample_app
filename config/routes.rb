323dee560ce5e2a349f0e56d45f633cc7aa8432e
mambocode <mambocode2@gmail.com>
2020-02-17 10:59:05 +0000

    5章終了 signupページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get "/signup",  to:'users#new'
   get "/contact", to:'static_pages#contact'
-  get "/help", to:'static_pages#help'
-  get "/about", to:'static_pages#about'
+  get "/help",    to:'static_pages#help'
+  get "/about",   to:'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end

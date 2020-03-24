7ee30c109a8fdbd9358e6f41586ffa4676b0aa03
mambocode <mambocode2@gmail.com>
2020-03-24 01:24:02 +0000

    9.2 remember_meチェックボックス実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user # user_url(user)と同じ
     else
       flash.now[:danger] = "Invalid email/password combination"

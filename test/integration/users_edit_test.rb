6a6c76acc78cd5a51950c861a8e6b72ac9683d4a
mambocode <mambocode2@gmail.com>
2020-03-24 08:28:46 +0000

    10.2 フレンドリーフォワーディングの実装 未ログイン状態でeditにgetリクエスト→ログインページ→編集画面に遷移するようにした

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -36,4 +36,24 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name,  @user.name
     assert_equal email, @user.email
   end
+  
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)                                                   # @userのユーザー編集ページを取得
+    assert_equal session[:forwarding_url],edit_user_url(@user)                  # getリクエストの結果、ログインしていなかったためログインページへリダイレクト
+                                                                                # →ログイン後編集ページへリダイレクトするためにsession[:forwarding_url]の中身を確認
+    log_in_as(@user)                                                            # @userでログイン
+    assert_redirected_to edit_user_path(@user)                                  # @userのユーザー編集ページへ移動する
+    assert session[:forwarding_url].nil?                                        # session[:forwarding_url]が空になっているか確認
+    name  = "Foo Bar"                                                           # 想定した名前とメールを設定
+    email = "foo@bar.com"                                                       
+    patch user_path(@user), params: { user: { name: name,                       # userIDをpatchリクエストで送信（更新）する
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?                                                     # エラー文が空じゃなければtrue
+    assert_redirected_to @user                                                  # michaelのユーザーidページへ移動できたらtrue
+    @user.reload
+    assert_equal name,  @user.name                                              # 想定の名前と@userの名前が一致していていたらtrue
+    assert_equal email, @user.email                                             # 想定のEmailと@userのメールが一致していていたらtrue
+  end
 end

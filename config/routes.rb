b1ed996afcf919cde8097094ea003efd02976274
mambocode <mambocode2@gmail.com>
2020-03-27 01:09:53 +0000

    activation用migrationファイルの生成 ルート使用のためにリソース指定

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   get "/help",    to:'static_pages#help'
   get "/about",   to:'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   root 'static_pages#home'
   
   resources :users
+  resources :account_activations, only: [:edit]
 end
